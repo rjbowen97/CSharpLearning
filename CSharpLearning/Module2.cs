711c84b4e9f616fbe59811edddb9879e360758ce
Richard Bowen <richard.bowen@listrak.com>
2018-01-16 11:45:29 -0500

    Added a module two class

---
+++ CSharpLearning/Module2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpLearning
+{
+    class Module2
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+    }
+}
