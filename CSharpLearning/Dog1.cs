253e3d553843e4497a6965a2c7ca806d7b0dee7f
Richard Bowen <richard.bowen@listrak.com>
2018-01-16 12:49:54 -0500

    Added partial classes

---
+++ CSharpLearning/Dog1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpLearning
+{
+    //Partial spreads the class out across Dog1 and Dog2
+    public partial class Dog
+    {
+        //Can only be used
+        private void Foo()
+        {
+
+        }
+    }
+}
