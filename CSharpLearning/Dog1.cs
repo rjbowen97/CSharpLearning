958fe8adbff57c1e115141b4c1b5e3de9704b86a
Richard Bowen <richard.bowen@listrak.com>
2018-01-16 12:55:28 -0500

    Added visibility modifiers

--- CSharpLearning/Dog1.cs
+++ CSharpLearning/Dog1.cs
@@ -7,10 +7,19 @@ namespace CSharpLearning
     //Partial spreads the class out across Dog1 and Dog2
     public partial class Dog
     {
-        //Can only be used
-        private void Foo()
-        {
 
-        }
+        //Only by the class
+        private void Foo() { }
+        
+        //Only by this and derived classes
+        //protected void Bar() { }
+
+        //Only in this assembly
+        internal void Bar() { }
+    }
+
+    class Poodle : Dog
+    {
+        void x() { this.}
     }
 }
