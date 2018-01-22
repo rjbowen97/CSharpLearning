d8aad9b32e00232d93fcac43309e9e0d1fa95c17
RJ Bowen <rjbowen97@gmail.com>
2018-01-22 09:09:42 -0500

    Added a module 3 class

---
+++ CSharpLearning/Module3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpLearning
+{
+    class Module3
+    {
+
+        public class y
+        {
+            public void x()
+            {
+                var animal = new Animal();
+                var dog = new Dog();
+                var poodle = new Poodle();
+
+                TakeAnimal(animal);
+            }
+            public void TakeAnimal(Animal a)
+            {
+                a.Temp = 10;
+                //var dog = (Dog)a;
+                var dog = a as Dog;
+
+                if (dog is Dog)
+                    dog.Name = "Daren";
+
+            }
+        }
+
+        public class Animal { public int Temp { get; set; } }
+        public class Dog : Animal { public string Name { get; set; }}
+        public class Poodle : Dog { public string Groomer { get; set; }}
+
+    }
+}
